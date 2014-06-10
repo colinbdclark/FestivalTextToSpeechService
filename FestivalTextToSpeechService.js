13c5d03fd4103ac328f7c4b609c73988368bc3c8
Colin Clark <colinbdclark@gmail.com>
2014-06-10 11:33:43 -0400

    FLUID-5405: Adds response headers for the returned audio file.

--- FestivalTextToSpeechService.js
+++ FestivalTextToSpeechService.js
@@ -1,5 +1,5 @@
 /*!
-FestivalTextToSpeechService.
+FLOE Festival Text to Speech Service
 
 Copyright 2013 OCAD University
 
@@ -14,13 +14,22 @@ var url = require("url");
 var spawn = require("child_process").spawn;
 
 http.createServer(function (req, res) {
+    res.setHeader("Content-Type", "audio/wave");
+    res.setHeader("Content-Disposition", "attachment; filename=tts.wav");
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+
     // Parse user query.
     var query = url.parse(req.url, true).query;
+
     // Extract the text from the |q| parameter.
     var echo = spawn("echo", [query.q], {stdio: ["pipe", "pipe", "pipe"]});
+
     // Send the text to text2wave and stream the speech back to the user.
-    var text2wave = spawn("/usr/bin/text2wave", [],
-        {stdio: ["pipe", "pipe", "pipe"]});
+    var text2wave = spawn("/usr/bin/text2wave", [], {
+        stdio: ["pipe", "pipe", "pipe"]
+    });
     echo.stdout.pipe(text2wave.stdin, {end: true});
     text2wave.stdout.pipe(res, {end: true});
 }).listen(8080);
